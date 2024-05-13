662825f25fbd0322034573536e9fd7a0b3ea6157
jwz <jwz.btc@outlook.com>
2024-05-13 14:45:45 +0800

    auto submit!!!

--- plot.py
+++ plot.py
@@ -1,7 +1,9 @@
+import sys
 import ccxt
-import requests
+import matplotlib
 import os
 import csv
+import matplotlib.pyplot
 import pandas as pd
 import matplotlib.pyplot as plt
 # os.chdir('/root/btcfuturedata')
@@ -28,6 +30,7 @@ delists = [
 def figure_plot(filename, symbol, basepath="./figures/"):
     # visualize the data in the csv file using pandas
     pdata = pd.read_csv(filename)
+    plt.title(symbol)
     pdata['timestamp'] = pd.to_datetime(pdata['timestamp'], unit='ms')
     pdata.set_index('timestamp', inplace=True)
     pdata["volume"] = pdata["sellVol"]+pdata["buyVol"]
@@ -38,43 +41,46 @@ def figure_plot(filename, symbol, basepath="./figures/"):
     openInterestHist.append("closePrice")
 
     plt.figure()
+    plt.title(symbol)
     pdata[openInterestHist].plot(subplots=True)
     plt.savefig(basepath+symbol+'.png', dpi=300)
 
-
+    matplotlib.pyplot.close('all')
     # # 大户账户多空比
-    # plt.figure()
-    # topLongShortAccountRatio="topacclongShortRatio,toplongAccount,topshortAccount".split(',')
-    # pdata[topLongShortAccountRatio].plot(subplots=True)
-    # plt.savefig(basepath+symbol+'_topLongShortAccountRatio.png', dpi=300)
+    plt.figure()
+    plt.title(symbol)
+    topLongShortAccountRatio="topacclongShortRatio,toplongAccount,topshortAccount".split(',')
+    topLongShortAccountRatio.append("closePrice")
+    pdata[topLongShortAccountRatio].plot(subplots=True)
+    plt.savefig(basepath+symbol+'_topLongShortAccountRatio.png', dpi=300)
     # # 大户持仓量多空比
-    # plt.figure()
-    # topLongShortPositionRatio="topposlongShortRatio,longPosition,shortPosition".split(',')
-    # pdata[topLongShortPositionRatio].plot(subplots=True)
-    # plt.savefig(basepath+symbol+'_topLongShortPositionRatio.png', dpi=300)
+    plt.figure()
+    plt.title(symbol)
+    topLongShortPositionRatio="topposlongShortRatio,longPosition,shortPosition".split(',')
+    topLongShortPositionRatio.append("closePrice")
+    pdata[topLongShortPositionRatio].plot(subplots=True)
+    plt.savefig(basepath+symbol+'_topLongShortPositionRatio.png', dpi=300)
+
     # # 多空持仓人数比
-    # plt.figure()
-    # globalLongShortAccountRatio="globallongShortRatio,globallongAccount,globalshortAccount".split(',')
-    # pdata[globalLongShortAccountRatio].plot(subplots=True)
-    # plt.savefig(basepath+symbol+'_globalLongShortAccountRatio.png', dpi=300)
+    plt.figure()
+    plt.title(symbol)
+    globalLongShortAccountRatio="globallongShortRatio,globallongAccount,globalshortAccount".split(',')
+    globalLongShortAccountRatio.append("closePrice")
+
+    pdata[globalLongShortAccountRatio].plot(subplots=True)
+    plt.savefig(basepath+symbol+'_globalLongShortAccountRatio.png', dpi=300)
 
 
-
-symbolist = []
-exchange = ccxt.binance({
-    'apiKey': '',
-    'secret': '',
-    'enableRateLimit': True,  # 启用请求限制
-    'options': {
-        'defaultType': 'future'
-    }
-})
-
-
-csvs = os.listdir('./csvs_merge')
-for csv in csvs:
-    symbol = csv.split('.')[0]
-    f = os.path.join('./csvs_merge', csv)
-    data = pd.read_csv(f)
+if len(sys.argv) > 1:
+    symbol = sys.argv[1]
+    f = os.path.join('./csvs', symbol+'.csv')
     figure_plot(f, symbol)
+    sys.exit(0)
+else:
+    csvs = os.listdir('./csvs')
+    for csv in csvs:
+        symbol = csv.split('.')[0]
+        f = os.path.join('./csvs', csv)
+        data = pd.read_csv(f)
+        figure_plot(f, symbol)
 

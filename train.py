d509a304b5efa7981cae93ae6410659334a14b8b
jwz <jwz.btc@outlook.com>
2024-05-17 07:45:08 +0800

    auto submit!!!

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 
 # 读取数据
-df = pd.read_csv('traindata/1000BONKUSDT.csv')  
+df = pd.read_csv('traindata/1000BONKUSDT.csv')
 
 # 数据预处理
 # 删除包含 NaN 值的行
@@ -24,6 +24,7 @@ def create_sequences(X, y, sequence_length=10):
     return np.array(X_sequences), np.array(y_sequences)
 
 
+# cols = "timestamp,sumOpenInterest,sumOpenInterestValue,topacclongShortRatio,toplongAccount,topshortAccount,topposlongShortRatio,longPosition,shortPosition,globallongShortRatio,globallongAccount,globalshortAccount,buySellRatio,sellVol,buyVol,openPrice,highPrice,lowPrice,closePrice".split(",")
 cols = "timestamp,sumOpenInterest,sumOpenInterestValue,longPosition,shortPosition,globallongShortRatio,globallongAccount,globalshortAccount,buySellRatio,sellVol,buyVol,buy".split(",")
 df = df[cols]
 
@@ -41,7 +42,6 @@ scaler = MinMaxScaler()
 x_shape = X_seq.shape
 X_normalized_flat = scaler.fit_transform(X_seq.reshape(-1, X_seq.shape[-1]))
 X_normalized = X_normalized_flat.reshape((-1, x_shape[1], x_shape[2]))
-
 
 # 划分训练集和测试集
 X_train, X_test, y_train, y_test = train_test_split(X_normalized, y_seq, test_size=0.2, random_state=42)
@@ -69,4 +69,5 @@ model.fit(X_train, y_train, epochs=1000, batch_size=32, validation_data=(X_test,
 # 在测试集上评估模型
 test_loss, test_acc = model.evaluate(X_test, y_test)
 print('Test accuracy:', test_acc)
+model.save('models/1000BONKUSDT.h5')
 

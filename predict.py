08db9af57f59df0db9efccbfb7eb3cf788072e9a
jwz <jwz.btc@outlook.com>
2024-05-17 21:31:09 +0800

    auto submit!!!

---
+++ predict.py
@@ -0,0 +1,121 @@
+from tensorflow.keras.models import load_model
+from sklearn.preprocessing import MinMaxScaler
+import pandas as pd
+import numpy as np
+from datetime import datetime
+
+import pbinance
+symbol = "1000BONKUSDT"
+period = "5m"
+limit = 10
+
+# 读取数据
+df = pd.read_csv('traindata/{}.csv'.format(symbol))
+
+# 数据预处理, 删除包含 NaN 值的行
+df.dropna(inplace=True)
+
+# 特征工程：将时间序列转换为序列数据
+def create_sequences(X, y, sequence_length=10):
+    X_sequences, y_sequences = [], []
+    for i in range(sequence_length, len(X)):
+        X_sequences.append(X[i-sequence_length:i])
+        y_sequences.append(y[i])  # 对应的标签就是当前行的y值
+    return np.array(X_sequences), np.array(y_sequences)
+
+
+def get_current_timestamp():
+    # 获取当前时间
+    current_time = datetime.now()
+
+    # 计算当前时间距离上一个能被 5 整除的分钟数
+    minutes = current_time.minute
+    nearest_five_minute = (minutes // 5) * 5
+
+    # 获取最近的五分钟的起始时间
+    nearest_time = current_time.replace(minute=nearest_five_minute, second=0, microsecond=0)
+
+    # 将最近的五分钟的起始时间转换为时间戳
+    nearest_timestamp = int(nearest_time.timestamp())*1000
+
+    # 输出最近的五分钟的时间戳
+    print(nearest_timestamp)
+    return nearest_timestamp
+
+
+cols = "timestamp,sumOpenInterest,sumOpenInterestValue,topacclongShortRatio,toplongAccount,topshortAccount,globallongShortRatio,globallongAccount,globalshortAccount,buy,openPrice,highPrice,lowPrice,closePrice".split(",")
+df = df[cols]
+
+# 将数据划分为特征和标签
+X = df.drop(columns=['timestamp', 'buy']).values
+y = df['buy'].values
+scaler = MinMaxScaler()
+X_seq, y_seq = create_sequences(X, y)
+
+# 归一化处理
+scaler = MinMaxScaler()
+x_shape = X_seq.shape
+X_normalized_flat = scaler.fit_transform(X_seq.reshape(-1, X_seq.shape[-1]))
+X_normalized = X_normalized_flat.reshape((-1, x_shape[1], x_shape[2]))
+
+
+# 根据 pbinance 获取数据
+binance = pbinance.Binance("", "")
+newdata = pd.DataFrame(columns=cols)
+
+openInterestHist = binance.um.market.get_openInterestHist(symbol=symbol, period=period, limit=limit)["data"]
+topLongShortAccountRatio = binance.um.market.get_topLongShortAccountRatio(symbol=symbol, period=period, limit=limit)["data"]
+topLongShortPositionRatio = binance.um.market.get_topLongShortPositionRatio(symbol=symbol, period=period, limit=limit)["data"]
+globalLongShortAccountRatio = binance.um.market.get_globalLongShortAccountRatio(symbol=symbol, period=period, limit=limit)["data"]
+# takerlongshortRatio = binance.um.market.get_takerlongshortRatio(symbol=symbol, period=period,limit=limit)["data"]
+prices = binance.um.market.get_markPriceKlines(symbol=symbol, interval=period, limit=limit)["data"]
+last_openInterestHist = openInterestHist[-1]
+last_toplongshortAccRatio = topLongShortAccountRatio[-1]
+last_toplongposRatio = topLongShortPositionRatio[-1]
+last_globalongshortAccratio = globalLongShortAccountRatio[-1]
+last_price = prices[-1]
+
+
+if last_openInterestHist["timestamp"] == last_toplongposRatio["timestamp"] == last_toplongshortAccRatio["timestamp"] == last_globalongshortAccratio["timestamp"] == last_price[0]:
+   sumOpenInterests = []
+   sumOpenInterestValues = []
+   topacclongShortRatios = []
+   toplongAccount = []
+   topshortAccounts = []
+   globallongShortRatios = []
+   globallongAccounts = []
+   globalshortAccounts = []
+   openPrices = []
+   highPrices = []
+   lowPrices = []
+   closePrices = []
+   
+   for oi, ta, tp, ga, ps in zip(openInterestHist, topLongShortAccountRatio, topLongShortPositionRatio, globalLongShortAccountRatio, prices):
+        # print(oi["timestamp"], ta["timestamp"], tp["timestamp"], ga["timestamp"], ps[0])
+        sumOpenInterests.append(float(oi["sumOpenInterest"]))
+        sumOpenInterestValues.append(float(oi["sumOpenInterestValue"]))
+        topacclongShortRatios.append(float(ta["longShortRatio"]))
+        toplongAccount.append(float(ta["longAccount"]))
+        topshortAccounts.append(float(ta["shortAccount"]))
+        globallongShortRatios.append(float(ga["longShortRatio"]))
+        globallongAccounts.append(float(ga["longAccount"]))
+        globalshortAccounts.append(float(ga["shortAccount"]))
+        openPrices.append(float(ps[1]))
+        highPrices.append(float(ps[2]))
+        lowPrices.append(float(ps[3]))
+        closePrices.append(float(ps[4]))
+   
+   vectors = np.column_stack((sumOpenInterests,sumOpenInterestValues,topacclongShortRatios, toplongAccount, topshortAccounts, globallongShortRatios, globallongAccounts, globalshortAccounts,openPrices, highPrices, lowPrices, closePrices))
+   vshape = vectors.shape
+   X_flat = scaler.fit_transform(vectors.reshape(-1, vectors.shape[-1]))
+   X_ = X_flat.reshape((1, vshape[0], vshape[1]))
+
+   # 加载模型
+   model = load_model('models/1000BONKUSDT.h5')
+
+   # 对新数据进行预测
+   predicted_labels = model.predict(X_)
+
+   # 输出预测结果
+   print(predicted_labels)
+
--- train.py
+++ train.py
@@ -7,48 +7,46 @@ from sklearn.preprocessing import MinMaxScaler
 
 from sklearn.model_selection import train_test_split
 
-# 读取数据
+# load data
 df = pd.read_csv('traindata/1000BONKUSDT.csv')
 
-# 数据预处理
-# 删除包含 NaN 值的行
+# data preprocessing, delete lines contain NaN
 df.dropna(inplace=True)
 
 
-# 特征工程：将时间序列转换为序列数据
+# to time series
 def create_sequences(X, y, sequence_length=10):
     X_sequences, y_sequences = [], []
     for i in range(sequence_length, len(X)):
         X_sequences.append(X[i-sequence_length:i])
-        y_sequences.append(y[i])  # 对应的标签就是当前行的y值
+        y_sequences.append(y[i]) 
     return np.array(X_sequences), np.array(y_sequences)
 
 
-# cols = "timestamp,sumOpenInterest,sumOpenInterestValue,topacclongShortRatio,toplongAccount,topshortAccount,topposlongShortRatio,longPosition,shortPosition,globallongShortRatio,globallongAccount,globalshortAccount,buySellRatio,sellVol,buyVol,openPrice,highPrice,lowPrice,closePrice".split(",")
-cols = "timestamp,sumOpenInterest,sumOpenInterestValue,longPosition,shortPosition,globallongShortRatio,globallongAccount,globalshortAccount,buySellRatio,sellVol,buyVol,buy".split(",")
+cols = "timestamp,sumOpenInterest,sumOpenInterestValue,topacclongShortRatio,toplongAccount,topshortAccount,globallongShortRatio,globallongAccount,globalshortAccount,buy,openPrice,highPrice,lowPrice,closePrice".split(",")
+
 df = df[cols]
 
-# 将数据划分为特征和标签
+# feature and label
 X = df.drop(columns=['timestamp', 'buy']).values
 y = df['buy'].values
 scaler = MinMaxScaler()
-np.save('scaler_params.npy', [scaler.min_, scaler.scale_])
 
-# 创建序列数据
+
 X_seq, y_seq = create_sequences(X, y)
 
-# 归一化处理
+# zero one
 scaler = MinMaxScaler()
 x_shape = X_seq.shape
 X_normalized_flat = scaler.fit_transform(X_seq.reshape(-1, X_seq.shape[-1]))
 X_normalized = X_normalized_flat.reshape((-1, x_shape[1], x_shape[2]))
 
-# 划分训练集和测试集
+# train test split
 X_train, X_test, y_train, y_test = train_test_split(X_normalized, y_seq, test_size=0.2, random_state=42)
 
 
 
-# 构建深层循环神经网络模型
+# LSTM
 model = Sequential([
     LSTM(units=128, return_sequences=True, input_shape=(X_train.shape[1], X_train.shape[2])),
     Dropout(0.2),
@@ -60,13 +58,10 @@ model = Sequential([
 ])
 
 
-# 编译模型
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-# 训练模型
 model.fit(X_train, y_train, epochs=1000, batch_size=32, validation_data=(X_test, y_test))
 
-# 在测试集上评估模型
 test_loss, test_acc = model.evaluate(X_test, y_test)
 print('Test accuracy:', test_acc)
 model.save('models/1000BONKUSDT.h5')

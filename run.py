34cf888cab066089730c338ed40a6f19e6279998
jwz <jwz.btc@outlook.com>
2024-05-19 09:34:55 +0800

    auto submit!!!

--- run.py
+++ run.py
@@ -6,10 +6,29 @@ import pandas as pd
 import datetime
 import ccxt
 import matplotlib.pyplot as plt
-
 from git import Repo
-os.chdir('/root/btcfuturedata')
+
 cols = "timestamp,sumOpenInterest,sumOpenInterestValue,topacclongShortRatio,toplongAccount,topshortAccount,topposlongShortRatio,longPosition,shortPosition,globallongShortRatio,globallongAccount,globalshortAccount,buySellRatio,sellVol,buyVol,openPrice,highPrice,lowPrice,closePrice".split(",")
+delists = [
+    "SRM",
+    "SLP",
+    "HNT",
+    "TOMO",
+    "USDC",
+    "CVC",
+    "CTK",
+    "BTS",
+    "BTCST",
+    "SC",
+    "DGB",
+    "RAY",
+    "ANT",
+    "FTT",
+    "FOOTBALL",
+    "BLUEBIRD",
+    "COCOS",
+    "STRAX"
+]
 
 
 def write_data(symbol, subpath="csvs", period="5m", limit=100):
@@ -135,90 +154,61 @@ def write_data(symbol, subpath="csvs", period="5m", limit=100):
     # return flagSumOpenInterestValue, flagTopacclongShortRatio, flagTopposlongShortRatio, flagGloballongShortRatio
 
 
+if __name__ == "__main__":
+    symbolist = []
+    exchange = ccxt.binance({
+        'apiKey': '',
+        'secret': '',
+        'enableRateLimit': True,  # 启用请求限制
+        'options': {
+            'defaultType': 'future'
+        }
+    })
+    symbols = []
+    markets = exchange.load_markets()
+    newHighList = [] 
+    for m in markets:
+        if m.endswith("/USDT:USDT"):
+            symbols.append(m)
+            s = m.split("/")[0]
+            symbol = m.split("/")[0]+"USDT"
+            flag = False
+            for delist in delists:
+                if delist in symbol:
+                    flag = True
+            if flag:
+                continue
+            # flagSumOpenInterestValue, flagTopacclongShortRatio, flagTopposlongShortRatio, flagGloballongShortRatio = write_data(symbol, subpath="csvs", limit=10)
+            flagSumOpenInterestValue, flagGloballongShortRatio = write_data(symbol, subpath="csvs", limit=10)
+            if flagSumOpenInterestValue:
+                newHighList.append(symbol+"-市值")
+            # if flagTopacclongShortRatio:
+                # newHighList.append(symbol+"-大户多空比")
+            # if flagTopposlongShortRatio:
+                # newHighList.append(symbol+"-大户持仓多空比")
+            if flagGloballongShortRatio:
+                newHighList.append(symbol+"-全网多空比")
 
-delists = [
-    "SRM",
-    "SLP",
-    "HNT",
-    "TOMO",
-    "USDC",
-    "CVC",
-    "CTK",
-    "BTS",
-    "BTCST",
-    "SC",
-    "DGB",
-    "RAY",
-    "ANT",
-    "FTT",
-    "FOOTBALL",
-    "BLUEBIRD",
-    "COCOS",
-    "STRAX"
-]
+    tz = pytz.timezone('Asia/Shanghai')
+    for s in newHighList:
+        print(s)
+        message = ""
+        data = pd.read_csv("./csvs/{}.csv".format(s.split("-")[0]))
+        last_data = data.iloc[-1]
 
+        for c in cols:
+            if c == "timestamp":
+                timestamp = int(float(last_data[c])) / 1000
+                dt = datetime.datetime.fromtimestamp(timestamp)
+                utc8_dt = dt.astimezone(tz)
+                utc8_time_str = utc8_dt.strftime('%Y-%m-%d %H:%M:%S')
+                message += "{}:\t{}\n".format("time", utc8_time_str)
+            else:
+                message += "{}:\t{}\n".format(c, last_data[c])
+        # requests.post("https://api.day.app/Rn4sQCRDQr3TYNaBuKoGZe/{}/{}".format(s, message))
+    repo_path = '/root/btcfuturedata'
+    repo = Repo(repo_path)
+    repo.git.add(all=True)
+    repo.git.commit("-m", "auto submit!!!")
+    repo.git.push()
 
-symbolist = []
-exchange = ccxt.binance({
-    'apiKey': '',
-    'secret': '',
-    'enableRateLimit': True,  # 启用请求限制
-    'options': {
-        'defaultType': 'future'
-    }
-})
-
-symbols = []
-markets = exchange.load_markets()
-newHighList = [] 
-
-for m in markets:
-    if m.endswith("/USDT:USDT"):
-        symbols.append(m)
-        s = m.split("/")[0]
-        symbol = m.split("/")[0]+"USDT"
-
-        flag = False
-        for delist in delists:
-            if delist in symbol:
-                flag = True
-        if flag:
-            continue
-
-        # flagSumOpenInterestValue, flagTopacclongShortRatio, flagTopposlongShortRatio, flagGloballongShortRatio = write_data(symbol, subpath="csvs", limit=10)
-        flagSumOpenInterestValue, flagGloballongShortRatio = write_data(symbol, subpath="csvs", limit=10)
-        if flagSumOpenInterestValue:
-            newHighList.append(symbol+"-市值")
-        # if flagTopacclongShortRatio:
-            # newHighList.append(symbol+"-大户多空比")
-        # if flagTopposlongShortRatio:
-            # newHighList.append(symbol+"-大户持仓多空比")
-        if flagGloballongShortRatio:
-            newHighList.append(symbol+"-全网多空比")
-
-tz = pytz.timezone('Asia/Shanghai')
-# if len(newHighList) == 0:
-    # newHighList.append("BTCUSDT")
-
-for s in newHighList:
-    print(s)
-    message = ""
-    data = pd.read_csv("./csvs/{}.csv".format(s.split("-")[0]))
-    last_data = data.iloc[-1]
-
-    for c in cols:
-        if c == "timestamp":
-            timestamp = int(float(last_data[c])) / 1000
-            dt = datetime.datetime.fromtimestamp(timestamp)
-            utc8_dt = dt.astimezone(tz)
-            utc8_time_str = utc8_dt.strftime('%Y-%m-%d %H:%M:%S')
-            message += "{}:\t{}\n".format("time", utc8_time_str)
-        else:
-            message += "{}:\t{}\n".format(c, last_data[c])
-    # requests.post("https://api.day.app/Rn4sQCRDQr3TYNaBuKoGZe/{}/{}".format(s, message))
-
-repo_path = '/root/btcfuturedata'
-repo = Repo(repo_path)
-repo.git.add(all=True)
-repo.git.commit("-m", "auto submit!!!")
-repo.git.push()
